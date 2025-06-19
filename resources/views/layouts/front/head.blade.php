5c2126b66cbe1d13c87da7a8db0c5fd3f2cbcd37
NiravViralDevelopment <viralode95745@gmail.com>
2025-06-19 21:06:51 +0530

    comment by

---
+++ resources/views/layouts/front/head.blade.php
@@ -0,0 +1,29 @@
+    <head>
+         <meta name="pagetitle" content="Indian Film Industry Directory :: Online Film Directory Of Actors, Actresses" />
+         <meta name="keywords" content="Actors, Bollywood,Malayalam Actors, Tamil Actors, Telugu Actors, Kannada Actors, Bengali Actors,Marathi Actors, Punjabi Actors, BHojpuri Actors" />
+         <meta name="description" content="Directory of the male actors, female actors from  Bollywood, Malayalam Actors, Tamil Actors, Telugu Actors, Kannada Actors, Bengali Actors, Marathi Actors, Punjabi Actors, Bhojpuri Actors" />
+         <meta charset="utf-8">
+         <meta name="viewport" content="width=device-width">
+         <link rel="shortcut icon" href="{{asset('front')}}/favicon.ico">
+         <title>ATOM Directory :: A Talent Organizing and Management Directory for New Actors and Models</title>
+         <link href="{{asset('front')}}/css/select2.css" rel="stylesheet">
+         <link href="{{asset('front')}}/css/bootstrap.min.css" rel="stylesheet">
+         <link href="{{asset('front')}}/css/style.css" rel="stylesheet">
+         <link rel="stylesheet" href="{{asset('frot')}}/vendor/pnotify/pnotify.custom.css" />
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+         <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+         <!--[if lt IE 9]>
+         <script src="js/html5shiv.js"></script>
+         <script src="js/respond.min.js"></script>
+         <![endif]-->
+         <script src="{{asset('front')}}/js/custom.modernizr.js" type="text/javascript" ></script>
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+         <script src="{{asset('front')}}/js/jquery.validate.js"></script>
+         <script>
+            window.jQuery || document.write('<script src="{{asset('front')}}/js/jquery.js"><\/script>')
+         </script>
+         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+         <script src="{{asset('front')}}/js/bootstrap.min.js"></script>
+         <script type="text/javascript" src="{{asset('front')}}/ckeditor/ckeditor.js"></script>
+      </head>
---
+++ resources/views/layouts/front/header.blade.php
@@ -0,0 +1,47 @@
+   <div class="top-listing header_inner">
+            <div class="container">
+               <div class="row">
+                  <div class="show768">
+                     <div class="listing-block topright inner_number">
+                        <div class="login_top">
+                           <ul>
+                              <li><a href=""><span class="glyphicon glyphicon-phone"></span> &nbsp;Whatsapp : +971 522 65 6679</a></li>
+                              <li><a href=""><span class="glyphicon glyphicon-phone"></span> Call Us : +971522656679</a></li>
+                              <li><a href="mailto:sales@dbuguae"><span class="glyphicon glyphicon-envelope"></span>&nbsp; Email : info@atomdirectory.com</a></li>
+                              <li class="green">
+                                 <!--<a href="{{asset('front')}}" class="reg">Registration</a> <span>/</span> -->
+                                 <a href="#" data-toggle="modal" data-target="#myModal" class="log btn_login" onclick="hideregisterdiv();">Registration <span>/</span> Log in</a>
+                              </li>
+                           </ul>
+                           <div class="clear"></div>
+                        </div>
+                        <div class="clear"></div>
+                     </div>
+                     <div class="clear"></div>
+                  </div>
+                  <div class="col-xs-12 col-sm-12 col-md-3">
+                     <div class="logo"><a href="https://atomdirectory.com/"><img src="{{asset('front/')}}/img/atom-header-logo.png" alt="" class="img-responsive dib" /></a></div>
+                  </div>
+                  <div class="col-xs-12 col-sm-12 col-md-9 text-right">
+                     <div class="listing-block topright inner_number hide768">
+                        <div class="login_top">
+                           <ul>
+                              <li><a href=""><span class="glyphicon glyphicon-phone"></span> &nbsp;Whatsapp : +971 522 6566 79</a></li>
+                              <li><a href="mailto:mr@ifid.in"><span class="glyphicon glyphicon-envelope"></span>&nbsp;Email: info@atomdirectory.com</a></li>
+                              <li class="green">
+                                 <!--<a href="{{asset('front')}}" class="reg">Registration</a> <span>/</span> -->
+                                 <a href="#" data-toggle="modal" data-target="#myModal" class="log btn_login">Registration <span>/</span> Log In</a>
+                              </li>
+                           </ul>
+                           <div class="clear"></div>
+                        </div>
+                        <div class="clear"></div>
+                     </div>
+                     <div class="clear"></div>
+                     <div class="results_serach">
+                        <div id="resultsfor"></div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
